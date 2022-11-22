ae764f1cd357b57e5123001d9f1f209aab33277d
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2022-11-22 13:13:25 +0500

    Done till practical 4

---
+++ activity-02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"\n\n\t\t JAMSHORO \n";
+	cout<<"\t===========================\n";
+	cout<<"\t\t Welcome To \n";
+	cout<<"\t Sindh Text Book Board";
+	return 0;
+}
