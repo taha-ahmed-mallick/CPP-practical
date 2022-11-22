fd78b492a2445b5032e1b8736cf0e1e7e5422a45
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2022-11-22 23:11:38 +0500

    done till a5

--- activity-05.cpp
+++ activity-05.cpp
@@ -19,5 +19,39 @@ int main()
 	cin>>science;
 	
 	cout<<"\n\n\t\t------------x--------------------x----------------x----------------x-----------\n\n";
+	cout<<"\t\t\t\t (Note Each Subject carries 100 marks)\n\n";
+	total=math+english+urdu+computer+science;
+	cout<<" \t\t\t\t The Total is equal to: "<<total<<"\n";
+	
+	percent = total*100/500;
+	
+	cout<<"\t\t\t\t The Percentage is equal to: "<<percent<<"%\n\n";
+	
+	if (percent >= 80)
+	{
+		cout<<"\t\t\t\t The Grade is A1 \n\n\n";
+	}
+	else if (percent >= 70)
+	{
+		cout<<"\t\t\t\t The Grade is A \n\n\n";
+	}
+	else if (percent >= 60)
+	{
+		cout<<"\t\t\t\t The Grade is B \n\n\n";
+	}
+	else if (percent >= 50)
+	{
+		cout<<"\t\t\t\t The Grade is C \n\n\n";
+	}
+	else if (percent >= 40)
+	{
+		cout<<"\t\t\t\t The Grade is D \n\n\n";
+	}
+	else if (percent >= 33)
+	{
+		cout<<"\t\t\t\t The Grade is E \n\n\n";
+	} else {
+		cout<<"\t\t\t\t Out of Range \n\n\n";
+	}
 	return 0;
 }
