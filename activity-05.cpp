67311fd0e78ee7be8bd86858ebcfcc87ef352979
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2022-11-22 23:06:26 +0500

    Not done a5

--- activity-05.cpp
+++ activity-05.cpp
@@ -3,5 +3,21 @@ using namespace std;
 
 int main()
 {
+	cout<<"\n\n\t\t\t    CALCULATE TOTAL, PERCENTAGE AND GRADE \n";
+	cout<<"\t\t===========================================================================\n\n";
+	int math, english, urdu, computer, science, total, percent;
+	
+	cout<<" \t\t\t\t Enter Marks for Maths: ";
+	cin>>math;
+	cout<<" \t\t\t\t Enter Marks for English: ";
+	cin>>english;
+	cout<<" \t\t\t\t Enter Marks for Urdu: ";
+	cin>>urdu;
+	cout<<" \t\t\t\t Enter Marks for Computer: ";
+	cin>>computer;
+	cout<<" \t\t\t\t Enter Marks for Science: ";
+	cin>>science;
+	
+	cout<<"\n\n\t\t------------x--------------------x----------------x----------------x-----------\n\n";
 	return 0;
 }
