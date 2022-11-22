3b576944baa2feceb2fd57a40c4e31ed287bb2de
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2022-11-22 23:19:07 +0500

    a7

---
+++ activity-06.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	cout<<"\n\t\t\t Enter 10 Characters \n";
+	cout<<"\n\t\t================================= \n";
+	char op[10];
+	for (int i = 0; i < 10; i++)
+	{
+		cout<<"\t\t";
+		op[i] = getch();
+		cout<<" "<<op[i]<<"\t\t\n\n";
+	}
+	return 0;
+}
